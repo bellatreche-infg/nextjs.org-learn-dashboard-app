05459227a35a2ef61b05165f71fe232c553a31ea
Brahim BELLATRECHE <b.bellatreche@dypix.com>
2025-04-12 16:54:30 +0100

    Inserting the data into your database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,8 @@
 'use server';
 import { z } from 'zod';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
@@ -17,6 +20,12 @@ export async function createInvoice(formData: FormData) {
         amount: formData.get('amount'),
         status: formData.get('status'),
       });
+      
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
 }
